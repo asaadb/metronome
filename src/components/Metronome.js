fccea84c4e7f48b0aee91c6c769ba7bb55b917dd
Asaad Budayr <asad_289@hotmail.com>
2019-03-29 14:52:43 -0700

    Feat: Add metronome functionality

--- src/components/Metronome.js
+++ src/components/Metronome.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import click1 from "../utils/click1.wav";
+import click2 from "../utils/click2.wav";
 
 class Metronome extends Component {
   state = {
@@ -6,6 +8,40 @@ class Metronome extends Component {
     count: 0,
     bpm: 100,
     beatsPerMeasure: 4
+  };
+  click1 = new Audio(click1);
+  click2 = new Audio(click2);
+  startStop = () => {
+    if (this.state.playing) {
+      // Stop the timer
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      // Start a timer with the current BPM
+      this.timer = setInterval(this.playClick, (60 / this.state.bpm) * 1000);
+      this.setState(
+        {
+          count: 0,
+          playing: true
+        },
+
+      );
+    }
+  };
+  playClick = () => {
+    const { count, beatsPerMeasure } = this.state;
+    // The first beat will have a different sound than the others
+    if (count % beatsPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+    // Keep track of which beat we're on
+    this.setState(state => ({
+      count: (state.count + 1) % state.beatsPerMeasure
+    }));
   };
   handleBpmChange = event => {
     const bpm = event.target.value;
@@ -25,7 +61,7 @@ class Metronome extends Component {
             onChange={this.handleBpmChange}
           />
         </div>
-        <button>{playing ? "Stop" : "Start"}</button>
+        <button onClick={this.startStop}>{playing ? "Stop" : "Start"}</button>
       </div>
     );
   }
